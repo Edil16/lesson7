8140a50275ea23cd85ef3de97956dbaadaa9b0af
Aigerim6939 <ajoldoshbekova21@gmail.com>
2022-04-22 14:55:08 +0600

    Initial commit

---
+++ src/com/company/Hero.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public abstract class Hero implements HavingSuperAbility {
+        private int health;
+        private int damage;
+        private String typeAbility;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getTypeAbility() {
+        return typeAbility;
+    }
+
+    public void setTypeAbility(String typeAbility) {
+        this.typeAbility = typeAbility;
+    }
+
+    @Override
+    public void superAbilityType() {
+
+    }
+}
---
+++ src/com/company/Magic.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Magic extends Hero implements HavingSuperAbility {
+    @Override
+    public void superAbilityType() {
+        super.superAbilityType();
+        System.out.println("Magic применил суперспособность Fire Ball");
+    }
+
+}
